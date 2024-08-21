402dcf020aba26192b4e49c144d7efa84728d6f8
felomousa <felogmousa@gmail.com>
2024-08-20 22:55:54 -0700

    BST Class defined

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main()
+struct TreeNode
 {
-    cout << "henlo :)";
-}
+    int nodeValue;
+    TreeNode *nodeLeft;
+    TreeNode *nodeRight;
+    TreeNode(int nodeValue) : nodeValue(nodeValue), nodeLeft(nullptr), nodeRight(nullptr) {};
+};
+
+class BinarySearchTree
+{
+private:
+    TreeNode *root;
+    TreeNode *insertNode(TreeNode *node, int value)
+    {
+        if (root == nullptr)
+        {
+            root = new TreeNode(value);
+            return root;
+        }
+
+        if (value < node->nodeValue)
+        {
+            node->nodeLeft = insertNode(node->nodeLeft, value);
+        }
+        else
+        {
+            node->nodeRight = insertNode(node->nodeRight, value);
+        }
+
+        return node;
+    }
+
+public:
+    void insertNode(int value) { root = insertNode(root, value); };
+    BinarySearchTree() { root = nullptr; }
+};
+
+int main() {};
